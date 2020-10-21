5af5391a23697975b2bc9e3ff0e0d86c3cf3a619
PraveenKumarSridhar <prasri.pk@gmail.com>
2020-10-22 03:05:04 +0530

    modified morning task such that it triggers a cron job at 9:30 everyday

--- src/morning_task.py
+++ src/morning_task.py
@@ -2,7 +2,16 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from personal_assistant import *
 import datetime
 
+# scheduler = BlockingScheduler()
+# scheduler.add_job(morning_events, 'interval', hours= 24, id='my_job_id')
+# print(datetime.datetime.now())
+from time import ctime
 scheduler = BlockingScheduler()
-scheduler.add_job(morning_events, 'interval', hours= 24, id='my_job_id')
-print(datetime.datetime.now())
+# scheduler.add_job(morning_events, 'interval', seconds=40, id='my_job_id')
+# print(datetime.datetime.now())
+@scheduler.scheduled_job('cron', hour=9,minute = 30)
+def timed_job_one():
+    morning_events()
+    print(ctime())
+
 scheduler.start()
