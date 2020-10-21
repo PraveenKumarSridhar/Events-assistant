7d7d7f425bbc5f26443561af595a858f41b58dd1
PraveenKumarSridhar <prasri.pk@gmail.com>
2020-10-22 03:58:25 +0530

    added global variables to control when to alert

--- src/morning_task.py
+++ src/morning_task.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from personal_assistant import *
+from global_variable import *
 import datetime
 
 # scheduler = BlockingScheduler()
@@ -9,7 +10,10 @@ from time import ctime
 scheduler = BlockingScheduler()
 # scheduler.add_job(morning_events, 'interval', seconds=40, id='my_job_id')
 # print(datetime.datetime.now())
-@scheduler.scheduled_job('cron', hour=9,minute = 30)
+alert_hr = int(ALERT_TIME.split(':')[0])
+alert_min = int(ALERT_TIME.split(':')[-1])
+
+@scheduler.scheduled_job('cron', hour=alert_hr ,minute = alert_min)
 def timed_job_one():
     morning_events()
     print(ctime())
